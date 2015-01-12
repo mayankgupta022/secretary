f782119a6f59c6459ca2d56fc063ce9e92d31a8d
Mayank Gupta <mayankg1992@gmail.com>
2015-01-12 22:32:22 +0530

    client organized router.js

--- client/modules/router.js
+++ client/modules/router.js
@@ -14,14 +14,28 @@ define(function (require) {
     return Backbone.Router.extend({
 
         routes: {
+
+            /**********/
+            /* COMMON */
+            /**********/
+
             "": "home",
             "blank": "blank",
+            "try/:id": "try",
+
+            /**********/
+            /* USER */
+            /**********/
+
             "changePass": "changePass",
             "login": "login",
             "logout": "logout",
             "signUp": "signUp",
             "updateInfo": "updateInfo",
-            "try/:id": "try",
+
+            /**********/
+            /* NOTES */
+            /**********/
 
             "notes": "notes",
             "notes/:id": "notes",
@@ -32,6 +46,9 @@ define(function (require) {
             "notes/trash": "notesTrash"
         },
 
+/**********/
+/* COMMON */
+/**********/
         initialize: function () {
 
             this.listenTo(this, "route", this.getInfo);
@@ -52,105 +69,6 @@ define(function (require) {
                 var homeView = new HomeView();
                 self.updateCurrentView(homeView);
                 $(homeView.render(menuView).el).appendTo($content);
-            });
-        },
-
-        changePass: function () {
-            var self = this;
-            require(["changePass/views/changePass"], function (ChangePassView) {
-                var changePassView = new ChangePassView();
-                self.updateCurrentView(changePassView);
-                $(changePassView.render(menuView).el).appendTo($content);
-            });
-        },
-
-        delNote: function (id) {
-            var self = this;
-            require(["notesDel/views/delNote"], function (DelNoteView) {
-                var delNoteView = new DelNoteView();
-                self.updateCurrentView(delNoteView);
-                $(delNoteView.render(id).el).appendTo($content);
-            });
-        },
-
-        login: function () {
-            var self = this;
-            require(["login/views/login"], function (LoginView) {
-                var loginView = new LoginView();
-                self.updateCurrentView(loginView);
-                $(loginView.render(menuView).el).appendTo($content);
-            });
-        },
-
-        logout: function () {
-            var self = this;
-            require(["logout/views/logout"], function (LogoutView) {
-                var logoutView = new LogoutView();
-                self.updateCurrentView(logoutView);
-                $(logoutView.render().el).appendTo($content);
-            });
-        },
-
-        newNote: function () {
-            var self = this;
-            require(["notesNew/views/newNote"], function (NewNoteView) {
-                var newNoteView = new NewNoteView();
-                self.updateCurrentView(newNoteView);
-                $(newNoteView.render().el).appendTo($content);
-            });
-        },
-
-        note: function (id) {
-            var self = this;
-            require(["notesNote/views/note"], function (NoteView) {
-                var noteView = new NoteView();
-                self.updateCurrentView(noteView);
-                $(noteView.render(menuView, id).el).appendTo($content);
-            });
-        },
-
-        notes: function (id = 0) {
-            var self = this;
-            require(["notes/views/notes"], function (NotesView) {
-                var notesView = new NotesView();
-                self.updateCurrentView(notesView);
-                $(notesView.render(menuView, id).el).appendTo($content);
-            });
-        },
-
-        restoreNote: function (id) {
-            var self = this;
-            require(["notesRestore/views/restoreNote"], function (RestoreNoteView) {
-                var restoreNoteView = new RestoreNoteView();
-                self.updateCurrentView(restoreNoteView);
-                $(restoreNoteView.render(id).el).appendTo($content);
-            });
-        },
-
-        signUp: function () {
-            var self = this;
-            require(["signUp/views/signUp"], function (SignUpView) {
-                var signUpView = new SignUpView();
-                self.updateCurrentView(signUpView);
-                $(signUpView.render(menuView).el).appendTo($content);
-            });
-        },
-
-        notesTrash: function () {
-            var self = this;
-            require(["notesTrash/views/trash"], function (NotesTrashView) {
-                var notesTrashView = new NotesTrashView();
-                self.updateCurrentView(notesTrashView);
-                $(notesTrashView.render(menuView).el).appendTo($content);
-            });
-        },
-
-        updateInfo: function () {
-            var self = this;
-            require(["updateInfo/views/updateInfo"], function (UpdateInfoView) {
-                var updateInfoView = new UpdateInfoView();
-                self.updateCurrentView(updateInfoView);
-                $(updateInfoView.render(menuView).el).appendTo($content);
             });
         },
 
@@ -190,6 +108,116 @@ define(function (require) {
             this.currentView=newView;
             this.currentView.delegateEvents(); // delegate events when the view is recycled
         }
+
+/**********/
+/* USER */
+/**********/
+
+        changePass: function () {
+            var self = this;
+            require(["changePass/views/changePass"], function (ChangePassView) {
+                var changePassView = new ChangePassView();
+                self.updateCurrentView(changePassView);
+                $(changePassView.render(menuView).el).appendTo($content);
+            });
+        },
+
+        login: function () {
+            var self = this;
+            require(["login/views/login"], function (LoginView) {
+                var loginView = new LoginView();
+                self.updateCurrentView(loginView);
+                $(loginView.render(menuView).el).appendTo($content);
+            });
+        },
+
+        logout: function () {
+            var self = this;
+            require(["logout/views/logout"], function (LogoutView) {
+                var logoutView = new LogoutView();
+                self.updateCurrentView(logoutView);
+                $(logoutView.render().el).appendTo($content);
+            });
+        },
+
+        signUp: function () {
+            var self = this;
+            require(["signUp/views/signUp"], function (SignUpView) {
+                var signUpView = new SignUpView();
+                self.updateCurrentView(signUpView);
+                $(signUpView.render(menuView).el).appendTo($content);
+            });
+        },
+
+        updateInfo: function () {
+            var self = this;
+            require(["updateInfo/views/updateInfo"], function (UpdateInfoView) {
+                var updateInfoView = new UpdateInfoView();
+                self.updateCurrentView(updateInfoView);
+                $(updateInfoView.render(menuView).el).appendTo($content);
+            });
+        },
+
+/**********/
+/* NOTES */
+/**********/
+
+        delNote: function (id) {
+            var self = this;
+            require(["notesDel/views/delNote"], function (DelNoteView) {
+                var delNoteView = new DelNoteView();
+                self.updateCurrentView(delNoteView);
+                $(delNoteView.render(id).el).appendTo($content);
+            });
+        },
+
+        newNote: function () {
+            var self = this;
+            require(["notesNew/views/newNote"], function (NewNoteView) {
+                var newNoteView = new NewNoteView();
+                self.updateCurrentView(newNoteView);
+                $(newNoteView.render().el).appendTo($content);
+            });
+        },
+
+        note: function (id) {
+            var self = this;
+            require(["notesNote/views/note"], function (NoteView) {
+                var noteView = new NoteView();
+                self.updateCurrentView(noteView);
+                $(noteView.render(menuView, id).el).appendTo($content);
+            });
+        },
+
+        notes: function (id) {
+
+            if(!id)
+                id = 0;
+            var self = this;
+            require(["notes/views/notes"], function (NotesView) {
+                var notesView = new NotesView();
+                self.updateCurrentView(notesView);
+                $(notesView.render(menuView, id).el).appendTo($content);
+            });
+        },
+
+        restoreNote: function (id) {
+            var self = this;
+            require(["notesRestore/views/restoreNote"], function (RestoreNoteView) {
+                var restoreNoteView = new RestoreNoteView();
+                self.updateCurrentView(restoreNoteView);
+                $(restoreNoteView.render(id).el).appendTo($content);
+            });
+        },
+
+        notesTrash: function () {
+            var self = this;
+            require(["notesTrash/views/trash"], function (NotesTrashView) {
+                var notesTrashView = new NotesTrashView();
+                self.updateCurrentView(notesTrashView);
+                $(notesTrashView.render(menuView).el).appendTo($content);
+            });
+        },
 
     });
 
